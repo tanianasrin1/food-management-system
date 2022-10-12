d050ad4d2637a20a129ac9b87e36c1fb672d2fa5
tanianasrin1 <tasrineva169@gmail.com>
2022-10-12 22:06:24 +0600

    slider part added

---
+++ js/app.js
@@ -0,0 +1,47 @@
+var swiper = new Swiper(".slider-content", {
+  slidesPerView: 3,
+  spaceBetween: 25,
+  loop: true,
+  centerSlider: 'true',
+  fade: 'true',
+  grabCursor: 'true',
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+    dynamicBullets: true,
+  },
+  breakpoints: {
+    "@0.00": {
+      slidesPerView: 1,
+      spaceBetween: 10,
+    },
+    "@0.75": {
+      slidesPerView: 2,
+      spaceBetween: 20,
+    },
+    "@1.00": {
+      slidesPerView: 3,
+      spaceBetween: 40,
+    },
+    "@1.50": {
+      slidesPerView: 4,
+      spaceBetween: 50,
+    },
+    navigation: {
+      nextEl: "swiper-button-next",
+      prevEl: "swiper-button-prev",
+    },
+    
+    breakpoints:{
+      0: {
+        slidesPerView: 1,
+      },
+      520: {
+        slidesPerView: 2,
+      },
+      950: {
+        slidesPerView: 3,
+      },
+    }
+  },
+});
